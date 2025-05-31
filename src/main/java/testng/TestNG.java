297ed147e21168def01a86945974f75e24081f56
Kunal <Kunal@DESKTOP-V7M61KO>
2025-05-31 16:20:33 +0530

    This is my first commit

---
+++ src/main/java/testng/TestNG.java
@@ -0,0 +1,12 @@
+package testng;
+
+import org.testng.annotations.Test;
+
+public class TestNG
+{
+@Test
+public void testcase1() {
+	
+	System.out.println("This is dummy");
+}
+}
---
+++ src/test/java/Selenium.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium {
+
+	public static void main(String[] args) throws InterruptedException
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		Thread.sleep(5000);
+		driver.manage().window().maximize();
+		driver.findElement(By.id("email")).sendKeys("Eshika@1234");
+		driver.findElement(By.name("pass")).sendKeys("291409");
+			
+	}
+
+}
