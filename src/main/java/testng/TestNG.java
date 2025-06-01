8607cbf43c91cd0c06c8bce1e52bf4108e265c66
Kunal <Kunal@DESKTOP-V7M61KO>
2025-06-01 12:42:57 +0530

    My first commit

--- src/main/java/testng/TestNG.java
+++ src/main/java/testng/TestNG.java
@@ -4,9 +4,10 @@ import org.testng.annotations.Test;
 
 public class TestNG
 {
-@Test
-public void testcase1() {
-	
-	System.out.println("This is dummy");
-}
+	@Test
+	public void testcase1() {
+		
+		System.out.println("This is dummy");
+	}
+
 }
